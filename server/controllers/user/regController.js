78e46e69b5d27fa6a44e3cabf32bbd69e02b7d45
bubinbei <bubinbei@yandex.ru>
2022-03-27 11:44:06 +0800

    0.5 registerUser

---
+++ server/controllers/user/regController.js
@@ -0,0 +1,23 @@
+import User from "../../models/userModel.js"
+import asyncHandler from "express-async-handler"
+// @desc   Register user
+// @route  POST /api/user
+// @access Public
+
+export const registerUser = asyncHandler(async(req, res) => {
+  const {email, password} = req.body
+
+  const isHaveUser = await User.findOne({email})
+
+  if (isHaveUser) {
+    res.status(400)
+    throw new Error("Данный пользователь уже зарегестрирован.")
+  }
+
+  const user = await User.create({
+    email,
+    password,
+  })
+  //Create token
+  res.json(user)
+})
